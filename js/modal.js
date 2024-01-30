7978af2511b751a9492a581014e79b6137cf1aed
jullis2003 <lisunjulia@gmail.com>
2024-01-30 22:41:50 +0200

    fix buges

--- js/modal.js
+++ js/modal.js
@@ -3,14 +3,36 @@ const modal = document.querySelector('.modal');
 const modalBtnOpen = document.querySelector('.modal-btn-open');
 const modalBtnClose = document.querySelector('.modal-btn-close');
 
-const toggleModal = () => modalBackdrop.classList.toggle('is-hidden');
-const closeModal = () => modalBackdrop.classList.add('is-hidden');
+const disableScroll = () => {
+  document.body.style.overflow = 'hidden';
+};
+const enableScroll = () => {
+  document.body.style.overflow = 'auto';
+};
+const toggleModal = () => {
+  modalBackdrop.classList.toggle('is-hidden');
+  if (!modalBackdrop.classList.contains('is-hidden')) {
+    disableScroll();
+  } else {
+    enableScroll();
+  }
+};
+const closeModal = () => {
+  modalBackdrop.classList.add('is-hidden');
+  enableScroll();
+};
 const closeModalIfClickedOutside = event => {
   if (!modal.contains(event.target) && !modalBtnOpen.contains(event.target)) {
     closeModal();
   }
 };
 
-modalBtnOpen.addEventListener('click', toggleModal);
-modalBtnClose.addEventListener('click', toggleModal);
+modalBtnOpen.addEventListener('click', () => {
+  toggleModal();
+  disableScroll();
+});
+modalBtnClose.addEventListener('click', () => {
+  toggleModal();
+  enableScroll();
+});
 document.addEventListener('click', closeModalIfClickedOutside);
