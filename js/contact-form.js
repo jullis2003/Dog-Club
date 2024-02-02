797a9199389a28af5152592117c42fd4bc22a251
jullis2003 <lisunjulia@gmail.com>
2024-02-02 23:24:10 +0200

    fixed and validation

---
+++ js/contact-form.js
@@ -0,0 +1,58 @@
+function validation(form) {
+  let result = true;
+
+  function createError(input, text) {
+    const parent = input.parentNode;
+    const errorText = document.createElement('p');
+
+    parent.classList.add('error');
+    errorText.classList.add('error-text');
+    errorText.textContent = text;
+
+    parent.append(errorText);
+  }
+
+  function removeError(input) {
+    const parent = input.parentNode;
+
+    if (parent.classList.contains('error')) {
+      parent.querySelector('error-text').remove();
+
+      parent.classList.remove('error');
+    }
+  }
+
+  form.querySelectorAll('.input-control').forEach(input => {
+    removeError(input);
+
+    if (input.dataset.required == 'true') {
+      if (input.value == '') {
+        removeError(input);
+        createError(input, "The field isn't filled!");
+
+        result = false;
+      }
+    }
+
+    // if (input.dataset.pattern) {
+    //   if (input.value == '') {
+    //     removeError(input);
+    //     createError(input, "The field isn't filled!");
+
+    //     result = false;
+    //   }
+    // }
+  });
+
+  return result;
+}
+
+document
+  .getElementById('contact-form')
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    if (validation(this) == true) {
+      alert('The form has been tested successfully!');
+    }
+  });
--- js/modal.js
+++ js/modal.js
@@ -2,24 +2,30 @@ const modalBackdrop = document.querySelector('.backdrop');
 const modal = document.querySelector('.modal');
 const modalBtnOpen = document.querySelector('.modal-btn-open');
 const modalBtnClose = document.querySelector('.modal-btn-close');
+const fixBlocks = document.querySelectorAll('.fix-block');
 
-const disableScroll = () => {
+const disableBodyScroll = () => {
+  let paddingOffset = window.innerWidth - document.body.offsetWidth + 'px';
   document.body.style.overflow = 'hidden';
+  document.body.style.paddingRight = paddingOffset;
+  fixBlocks.forEach(el => (el.style.paddingRight = paddingOffset));
 };
-const enableScroll = () => {
-  document.body.style.overflow = 'auto';
+const enableBodyScroll = () => {
+  document.body.style.overflow = '';
+  document.body.style.paddingRight = 0;
+  fixBlocks.forEach(el => (el.style.paddingRight = 0));
 };
 const toggleModal = () => {
   modalBackdrop.classList.toggle('is-hidden');
   if (!modalBackdrop.classList.contains('is-hidden')) {
-    disableScroll();
+    disableBodyScroll();
   } else {
-    enableScroll();
+    enableBodyScroll();
   }
 };
 const closeModal = () => {
   modalBackdrop.classList.add('is-hidden');
-  enableScroll();
+  enableBodyScroll();
 };
 const closeModalIfClickedOutside = event => {
   if (!modal.contains(event.target) && !modalBtnOpen.contains(event.target)) {
@@ -27,12 +33,6 @@ const closeModalIfClickedOutside = event => {
   }
 };
 
-modalBtnOpen.addEventListener('click', () => {
-  toggleModal();
-  disableScroll();
-});
-modalBtnClose.addEventListener('click', () => {
-  toggleModal();
-  enableScroll();
-});
+modalBtnOpen.addEventListener('click', toggleModal);
+modalBtnClose.addEventListener('click', toggleModal);
 document.addEventListener('click', closeModalIfClickedOutside);
